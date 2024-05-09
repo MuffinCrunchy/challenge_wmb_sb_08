79018f3e982f4ca3a7b56043beceb780d6b60b4c
Baihaqi Muhammad <baihaqi.m551@gmail.com>
2024-05-09 12:48:03 +0700

    initial commit

---
+++ src/main/java/com/muffincrunchy/challenge_wmb_sb_08/ChallengeWmbSb08Application.java
@@ -0,0 +1,13 @@
+package com.muffincrunchy.challenge_wmb_sb_08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeWmbSb08Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeWmbSb08Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/muffincrunchy/challenge_wmb_sb_08/ChallengeWmbSb08ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muffincrunchy.challenge_wmb_sb_08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeWmbSb08ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
